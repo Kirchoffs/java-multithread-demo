0f33ac65d4b5f796bda72556c334be9138137811
Yonghao Shu <123shuyonghao@gmail.com>
2023-04-15 17:56:20 -0700

    Add ForEach CountedCompleter demo

---
+++ src/main/java/org/syh/demo/java/multithreading/forkjoin/ForEachCountedCompleterDemoV1.java
@@ -0,0 +1,49 @@
+package org.syh.demo.java.multithreading.forkjoin;
+
+import java.util.concurrent.CountedCompleter;
+
+public class ForEachCountedCompleterDemoV1 {
+    public static void main(String[] args) {
+        String[] arr = new String[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + "";
+        }
+        ForEach.forEach(arr, (str) -> System.out.println(str + str));
+    }
+}
+
+interface CustomizedOperation<E> {
+    void apply(E e);
+}
+
+class ForEach<E> extends CountedCompleter<Void> {
+    public static <E> void forEach(E[] array, CustomizedOperation<E> op) {
+        new ForEach<E>(null, array, op, 0, array.length).invoke();
+    }
+ 
+    final E[] array; 
+    final CustomizedOperation<E> op; 
+    final int lo, hi;
+
+    private ForEach(CountedCompleter<?> p, E[] array, CustomizedOperation<E> op, int lo, int hi) {
+        super(p);
+        this.array = array; 
+        this.op = op; 
+        this.lo = lo; 
+        this.hi = hi;
+    }
+ 
+    public void compute() {
+        if (hi - lo >= 2) {
+            int mid = lo + (hi - lo) / 2;
+            setPendingCount(2);
+            new ForEach(this, array, op, mid, hi).fork();
+            new ForEach(this, array, op, lo, mid).fork();
+        } else if (hi > lo) {
+            op.apply(array[lo]);
+        }
+            
+        tryComplete();
+    }
+}
+ 
